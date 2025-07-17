e1b421a23ae9cb11f6ba1ae461a617fde8d4589d
soonwoolog <tnsdn94560@gmail.com>
2025-07-17 16:23:24 -0400

    Update package.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = { title: "nextjs-notion-starter" };
+export const metadata: Metadata = { title: "nextjs-notion-intergration" };
 
 export default function RootLayout({
   children
